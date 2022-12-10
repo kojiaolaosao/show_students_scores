b8774763c3a8058a0bea8d521eedb5dda131a3c5
qiwentao <1547936042@qq.com>
2022-12-10 21:47:53 +0800

    create GradeRecordVO

--- src/main/java/com/showscores/vo/GradeRecordVO.java
+++ src/main/java/com/showscores/vo/GradeRecordVO.java
@@ -5,6 +5,18 @@ import lombok.Data;
 
 @Data
 public class GradeRecordVO extends BaseEntity {
-//    姓名	总分	语文	数学	英语	附加	物理	地理	历史	政治	化学	生物	名次
-
+    //    姓名	总分	语文	数学	英语	附加	物理	地理	历史	政治	化学	生物	名次
+    private String name;
+    private Integer total_score;
+    private Integer languages;
+    private Integer maths;
+    private Integer englishes;
+    private Integer addition;
+    private Integer physicals;
+    private Integer geography;
+    private Integer history;
+    private Integer politics;
+    private Integer chemicals;
+    private Integer biological;
+    private Integer student_id;
 }
